b8724e19d5ed44d968db88aae10f583c8660334f
dufgu888 <dufgu888@gmail.com>
2022-07-28 00:54:30 +0300

    committest

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -161,6 +161,9 @@ export default function Menu() {
               <li>
                 <Link to="/">Modelli</Link>
               </li>
+              <li>
+                
+              </li>
             </ul>
           </div>
         </li>
